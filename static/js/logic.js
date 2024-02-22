677cc48ba8c0610861437012123473ce0133473d
Angie Foust <angiecfoust@gmail.com>
2024-02-21 21:04:45 -0600

    working on legend

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,82 +1,126 @@
-    //create map
-    let myMap = L.map("map", {
-      center: [
-        37.09, -95.71
-      ],
-      zoom: 3
-    });
-  
-  //create tile layers
-  let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
-
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-
-  let baseMaps = {
-    "Street Map": streetmap,
-    "Topographic Map": topo
-  };
-
+//create map  ////// THIS MAY NEED TO GO IN THE DRAW MAP FUNCTION
+//let myMap = L.map("map", {
+  //center: [
+    //37.09, -95.71
+  //],
+  //zoom: 3,
+  //layers: [streetmap, earthquakes]
+//});
 
 // store our endpoint as a variable
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 // GET request to the query URL
 d3.json(url).then(function (data) {
-  main(data.features);
+  createFeatures(data.features);
 });
 
-
-// create function 'main'
-function main(features) {
-
-  // set up color conditional based on depth
-  for (let i = 0; i < features.length; i++) {
-    let color = "";
-    let depth = features[1].geometry.coordinates[2];
-    if (depth > 65) {
-      color = "#880E4F";
-    }
-    else if (depth > 50) {
-      color = "#C2185B";
-    }
-    else if (depth > 35) {
-      color = "#E91E63";
-    }
-    else if (depth > 20) {
-      color = "#F06292";
-    }
-    else if (depth > 5) {
-      color = "#F8BBD0";
-    }
-    // set up radius based on magnitude
-    let radius = features[i].properties.mag * 5;
-
+//create map function
+function drawMap(earthquakes) {
+    //create tile layers
+    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+  
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
     
-    //create circle markers
-    var earthquakes = L.geoJson(features, {
-      onEachFeature: (feature, layer) => {
-        L.circle(features.geometry, {
-          color: color,
-          fillColor: color,
-          fillOpacity: 0.75,
-          radius: radius
-        }).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</br>
-        Magnitude: ${feature.properties.mag}</p>`);
-      }
-    }
-      )};
-    // create overlay maps
-  let overlayMaps = {
-  Earthquakes: earthquakes
-  };
+    //create basemap layer
+    let baseMaps = {
+      "Street Map": streetmap,
+      "Topographic Map": topo
+    };
 
-  // add control layer
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+    //create overlay
+    let overlayMaps = {
+      Earthquakes: earthquakes
+      };
+
+    //create map
+    let myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 3,
+      layers: [streetmap, earthquakes]
+    });
+
+    // add control layer
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
+    // make a legend
+   let legend = L.control({position: "bottomright"});
+   legend.onAdd = function (myMap) {
+    let div = L.DomUtil.create("div", "info legend");
+    grades = [-5, 5, 15, 25, 35, 45]
+    labels = [];
+    for (let i = 0; i < categories.length; i++) {
+      div.innerHTML +=
+      labels.push(
+        "<i style="background:" + setColor(depth));
+    }
+    div.innerHTML = labels.join('<br>');
+    return div;
+   };
+   legend.addTo(myMap);
+
+}
+
+
+// set up color function/conditional based on depth
+function setColor(depth) {
+    let color = "";
+    //let depth = features[1].geometry.coordinates[2]; //this may need to be defined elsewhere
+    if (depth > 45) 
+      color = "#4A235A";
+    else if (depth > 35) 
+      color = "#6C3483";
+    else if (depth > 25) 
+      color = "#8E44AD";
+    else if (depth > 15) 
+      color = "#BB8FCE";
+    else if (depth > 5) 
+      color = "#E8DAEF";
+    else if (depth > -5)
+      color = "#F4ECF7"
+    return color;
+ } 
+
+
+// create features function (note to me- see 'oneach' in activity 3.10)
+function createFeatures(features) {
+  // create the 'oneach' and make a bindPopup
+  function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</br>
+    Magnitude: ${feature.properties.mag}</br>Depth: ${feature.geometry.coordinates[2]}</p>`);
   }
+
+  //create circle markers function- radius based on magnitude
+  function createMarkers(feature, location) {
+    let depth = feature.geometry.coordinates[2]; 
+    let mag = feature.properties.mag;
+    return L.circleMarker (location, {
+      color: setColor(depth),
+      fillColor: setColor(depth),
+      fillOpacity: 0.75,
+      weight: 1,
+      radius: mag * 4
+    })
+
+  }
+
+  //create geoJson layer
+  let earthquakes = L.geoJson(features, {
+    onEachFeature: onEachFeature,
+    pointToLayer: createMarkers
+  });
+
+  //call the map function!
+  drawMap(earthquakes);
+
+}
+
 
